8dd817800a8a3dfe04db0c27ce785d9e2148a5bf
GU15 <119710589+Lynxguerba@users.noreply.github.com>
2026-03-18 19:23:23 +0800

    commit changes
     Confirming fix with regeneration and restart

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,10 +6,27 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        then: function () {
+            Route::middleware('web')
+                ->group(base_path('routes/admin.php'));
+            Route::middleware('web')
+                ->group(base_path('routes/instructor.php'));
+            Route::middleware('web')
+                ->group(base_path('routes/student.php'));
+            Route::middleware('web')
+                ->group(base_path('routes/adviser.php'));
+            Route::middleware('web')
+                ->group(base_path('routes/panelist.php'));
+            Route::middleware('web')
+                ->group(base_path('routes/dean.php'));
+            Route::middleware('web')
+                ->group(base_path('routes/program_chairperson.php'));
+        },
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
