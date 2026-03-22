3a1ba20efacb08d9b86da3fa5478406a2761defc
GU15 <119710589+Lynxguerba@users.noreply.github.com>
2026-03-22 23:49:30 +0800

    commit change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\PreventBackHistory;
+use App\Http\Middleware\RecordAdminAuditLog;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -35,6 +36,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
             PreventBackHistory::class,
+            RecordAdminAuditLog::class,
         ]);
 
         $middleware->alias([
