f5fbb056343f1b485816838f8a7f51ad08e9b8b5
GU15 <119710589+Lynxguerba@users.noreply.github.com>
2026-03-29 20:12:35 +0800

    commit new files

---
+++ app/Http/Requests/Panelist/UpdatePanelistAvailabilityRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Panelist;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePanelistAvailabilityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'is_available' => ['required', 'boolean'],
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'is_available.required' => 'Select the availability status.',
+            'is_available.boolean' => 'Availability must be open or closed.',
+        ];
+    }
+}
---
+++ app/Http/Requests/Panelist/UpdatePanelistProgramUtilitiesRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Panelist;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePanelistProgramUtilitiesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'programs' => ['required', 'array', 'min:1'],
+            'programs.*.program' => ['required', 'string', 'max:50'],
+            'programs.*.max_groups' => ['required', 'integer', 'min:0', 'max:50'],
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'programs.required' => 'Add at least one program utility.',
+            'programs.array' => 'Program utilities must be a list.',
+            'programs.*.program.required' => 'Program name is required.',
+            'programs.*.program.max' => 'Program name must be 50 characters or less.',
+            'programs.*.max_groups.required' => 'Set the maximum number of groups.',
+            'programs.*.max_groups.integer' => 'Maximum groups must be a number.',
+            'programs.*.max_groups.min' => 'Maximum groups must be zero or more.',
+            'programs.*.max_groups.max' => 'Maximum groups must be 50 or less.',
+        ];
+    }
+}
